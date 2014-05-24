83f5581a310fe881a47bbc9bc80250dda9f74292
Brian Ford <btford@umich.edu>
2014-05-24 14:15:30 -0700

    feat: add columns

--- bin.js
+++ bin.js
@@ -1,3 +1,28 @@
-var search = require('./lib/search-and-analyze-readme');
+#!/usr/bin/env node
 
-search(process.argv.slice(2), console.log.bind(console));
+var search    = require('./lib/search-and-analyze-readme');
+var columnify = require('columnify');
+
+var terms = process.argv.slice(2);
+
+var options = {
+  config: {
+    name: { maxWidth: 40, truncate: false, truncateMarker: '' },
+    description: { maxWidth: 60 }
+  }
+};
+
+search(terms, function (err, packages) {
+  var sortedPackages = packages.map(function (package) {
+    return {
+      name         : package.name,
+      description  : package.description,
+      score        : Math.floor((package.readmeScore.headers +
+                      package.readmeScore.writing) * 50)
+    };
+  }).
+  sort(function (a, b) {
+    return b.score - a.score;
+  });
+  console.log(columnify(sortedPackages, options));
+});
