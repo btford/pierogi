59309a6b5c0e0ba28feba5e3df475781f55b2c74
Brian Ford <btford@umich.edu>
2014-06-15 14:24:32 -0700

    feat: add lint command
    Closes #9

--- bin.js
+++ bin.js
@@ -1,30 +1,44 @@
 #!/usr/bin/env node
 
-var search    = require('./lib/search-and-analyze-readme');
-var columnify = require('columnify');
+var search    = require('./lib/search');
+var view      = require('./lib/view');
+var output    = require('./lib/output-search');
+var npmClient = require('./lib/npm-client');
 
 var terms = process.argv.slice(2);
 
-var options = {
-  config: {
-    name: { maxWidth: 40, truncate: false, truncateMarker: '' },
-    description: { maxWidth: 60 }
+var actions = {
+  lint: function (err, npm) {
+    view(terms, npm, function (err, out) {
+      console.log(out.name + ': ' + out.score + '/100');
+      printScore(out.readmeScore);
+      printScore(out.packageScore);
+    });
+  },
+
+  search: function (err, npm) {
+    search(terms, npm, function (err, packages) {
+      err ? console.log(err) : output(packages);
+    });
   }
 };
 
-search(terms, function (err, packages) {
-  var sortedPackages = packages.map(function (package) {
-    return {
-      name         : package.name,
-      description  : package.description,
-      score        : Math.floor(100 / 3 *
-                      (package.readmeScore.headers +
-                      package.readmeScore.writing +
-                      package.packageScore.maintainers))
-    };
-  }).
-  sort(function (a, b) {
-    return b.score - a.score;
+var action;
+
+if ((action = terms.length && terms.shift()) && actions[action]) {
+  npmClient(actions[action]);
+} else {
+  console.log('action' + (action ? (' "' + action + '"') : '') + ' not found');
+  process.exit(1);
+}
+
+function printScore (score) {
+  Object.keys(score).forEach(function (criteria) {
+    if (score[criteria].suggestions && score[criteria].suggestions.length > 0) {
+      console.log(' ' + criteria);
+      score[criteria].suggestions.forEach(function (suggestion) {
+        console.log('  - ' + (typeof suggestion === 'object' ? suggestion.reason : suggestion));
+      });
+    }
   });
-  console.log(columnify(sortedPackages, options));
-});
+}
